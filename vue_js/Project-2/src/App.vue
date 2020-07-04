1457c7932d92888d45e310a7594fea87d621c8f9
Bishwajit-Bhattacharjee <bishwajit0020@gmail.com>
2020-07-04 18:09:43 +0600

    Second project from the course

---
+++ vue_js/Project-2/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+	<div id="container">
+		<app-progress-bar :quoteCount='quotes.length' :maxQuotes="maxQuotes"></app-progress-bar>
+		<app-new-quote @quoteAdded="newQuote"></app-new-quote>
+		<app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+		<div class="col-sm-12 text-center">
+			<div class="alert alert-info">
+				Click on the Quote to delete
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+	import NewQuote from './components/NewQuote.vue';
+	import ProgressBar from './components/ProgressBar.vue';
+
+	export default {
+		data: function() {
+			return {
+				quotes : [
+					'Just a Quote to see something'
+				],
+				maxQuotes : 10
+			}
+		},
+		methods :{
+			newQuote(quote){
+				if(this.quotes.length >= this.maxQuotes){
+					return alert('Please delete Quotes first');
+				}
+				this.quotes.push(quote);
+			},
+			deleteQuote(index){
+				this.quotes.splice(index,1);
+			}
+		},
+		components: { 
+			'app-quote-grid' : QuoteGrid,
+			'app-new-quote' : NewQuote,
+			'app-progress-bar': ProgressBar
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ vue_js/Project-2/src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='row'>
+        <my-quote v-for="(quote,index) in quotes" :key="index" @click.native="deleteQuote(index)">
+            {{ quote }}
+        </my-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from "./Quote.vue";
+    export default {
+        props : ['quotes'],
+        components : {
+            'my-quote' : Quote
+        },
+        methods: {
+            deleteQuote(index){
+                this.$emit('quoteDeleted',index);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
